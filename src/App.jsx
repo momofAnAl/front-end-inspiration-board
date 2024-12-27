f328f6471280be509ceeb7b4d9b13018e89bb9a1
anhtran <anhtr077@gmail.com>
2024-12-27 11:20:29 -0800

    update states for App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,15 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useEffect, useState } from 'react'
+
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [board, setBoards] = useState([])
+  const [selectedBoard, setselectedBoard] = useState(null)
+  const [cards, setCards] = useState([])
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
+      <h1>Inspiration Board App</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
